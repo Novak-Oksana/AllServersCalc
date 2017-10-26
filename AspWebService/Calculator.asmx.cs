a3f5107c75bd5cd1c31baeaee57a4715ce37255d
Novak-Oksana <oksana.babaeva@gmail.com>
2017-10-27 01:51:27 +0300

    Add project files.

---
+++ AspWebService/Calculator.asmx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace AspWebService
+{
+    /// <summary>
+    /// Summary description for Calculator
+    /// </summary>
+    [WebService(Namespace = "http://localhost:9999/Calculator.asmx")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class Calculator : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void Calc(string num1, string num2, string op)
+        {
+            int res = 0;
+            int n1 = Convert.ToInt32(num1);
+            int n2 = Convert.ToInt32(num2);
+            switch (op)
+            {
+                case "-": res = n1 - n2; break;
+                case "p": res = n1 + n2; break;
+                case "*": res = n1 * n2; break;
+                case "/": res = n1 / n2; break;
+            }
+            Object obj = new
+            {
+                Result = res
+            };
+
+            var oSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            Context.Response.Write(oSerializer.Serialize(obj));
+
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcServer server = new CalcServer("127.0.0.1", 8080);
+            server.Start();
+        }
+    }
+}
